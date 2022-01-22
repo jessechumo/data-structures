36364c6e37e7e679fa9d3c8ef318e0325f1e5900
Jesse-Bett <jessekip21@gmail.com>
2022-01-22 14:50:04 +0300

    added js object

---
+++ index.js
@@ -0,0 +1,13 @@
+//Objects
+let cat = { name:"fluffy",
+            age:3,
+            eyeColor:"blue",
+            favoriteMeal:{
+                Morning:"Fish",
+                Lunch:"Chicken"
+            }}
+
+function chicken() {console.log(cat.favoriteMeal.Lunch)}
+chicken()
+
+
