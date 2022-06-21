4a2520deb994e864a64b939c8192dc6d7c4b0ee8
jesse kiprotich <jessekip21@gmail.com>
2022-06-21 15:24:51 +0300

    Added array methods

--- Data Structures/Arrays.js
+++ Data Structures/Arrays.js
@@ -13,3 +13,24 @@ console.log(myFavoriteNumbers[2])//12 is logged to the console
 const numbers =[4,9,16,25]
 const newArr = numbers.map(Math.sqrt)
 console.log(newArr)// A new array showing the square roots of elements of the array "numbers" is logged to the console.
+
+
+
+[1, 2, 3].push(4); // [1, 2, 3, 4]
+[1, 2, 3].pop(); // [1, 2]
+[1, 2, 3].shift(); // [2, 3]
+[1, 2, 3].unshift(0); // [0, 1, 2, 3]
+['a', 'b'].concat('c'); // ['a', 'b', 'c']
+['a', 'b', 'c'].join('-'); // a-b-c
+['a', 'b', 'c'].slice(1); // ['b', 'c']
+['a', 'b', 'c'].indexOf('b'); // 1
+['a', 'b', 'c'].includes('c'); // true
+[3, 5, 6, 8].find((n) => n % 2 === 0); // 6
+[2, 4, 3, 5].findIndex((n) => n % 2 !== 0); // 2
+[3, 4, 8, 6].map((n) => n * 2); // [6, 8, 16, 12]
+[1, 4, 7, 8].filter((n) => n % 2 === 0); // [4, 8]
+[2, 4, 3, 7].reduce((acc, cur) => acc + cur); // 16
+[2, 3, 4, 5].every((x) => x < 6); // true
+[3, 5, 6, 8].some((n) => n > 6); // true
+[1, 2, 3, 4].reverse(); // [4, 3, 2, 1]
+[3, 5, 7, 8].at(-2); // 7
